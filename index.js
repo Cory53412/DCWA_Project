d63d0961ff10c28d7508e7e32778e9691853ebe0
Cory <coryodonoghue1@gmail.com>
2020-12-28 19:59:37 +0000

    update, add & mongo not working

--- index.js
+++ index.js
@@ -4,100 +4,79 @@ const port = 3000;
 var bodyParser = require('body-parser')
 var MongoDAO = require('./MongoDAO');
 var mySQLDAO = require('./mySQLDAO');
-var countries = require ('./countries');
 
-//use of body parser for web app functionality    
 app.use(bodyParser.urlencoded({ extended: false }))
-
-app.set('view engine' , 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render("home")
 })
 
-//////////////////////////////////////////////////////////////////////////all city functions
 app.get('/listCities', (req, res) => {
     mySQLDAO.getCities()
         .then((result) => {
             console.log(result)
-            res.render('showCities',{cities:result});
+            res.render('showCities', { cities: result });
         })
-        .catch((err)=>{
+        .catch((err) => {
             res.send(err);
         })
 })
 
-app.get('/listCities/:city', (req,res)=>{
-    mySQLDAO.getCity(req.params.city )
-    .then((result)=>{
-        if(result.length > 0){
-            res.render('cityDetails', {cities: result})
-        }
-        else{
-            res.send("<h3> no such city with id: " + req.params.city)      
-      }
-    })
-    .catch((err)=>{
-        console.log("NOK");
-        console.log(err);
-        res.send(err);
-    })
+app.get('/listCities/:city', (req, res) => {
+    mySQLDAO.getCity(req.params.city)
+        .then((result) => {
+            if (result.length > 0) {
+                res.render('cityDetails', { cities: result })
+            }
+            else {
+                res.send("<h3> no such city with id: " + req.params.city)
+            }
+        })
+        .catch((err) => {
+            console.log("NOK");
+            console.log(err);
+            res.send(err);
+        })
 })
 
-
-
-////////////////////////////////////////////////////////////////all country functions
 app.get('/listCountries', (req, res) => {
-    countries.getCountries()
+    mySQLDAO.getCountries()
         .then((result) => {
             console.log(result);
-            res.render('showCountries',{country:result});
+            res.render('showCountries', { country: result });
         })
-        .catch((err)=>{
+        .catch((err) => {
             res.send(err);
         })
 })
 
-app.get('/listCountries/:country' , (req,res)=>{
-    countries.deleteCountry(req.params.country)
-    .then((result)=>{
-        if(result.affectedRows == 0){
-            res.send("<h3>Country: " + req.params.country + " doesnt exist")
-        }
-        else{
-            res.send("<h3> Country: " + req.params.country + " deleted" )
-        }
-        res.send(result);
-    })
-    .catch((err)=>{
-        if(err.code == "ER_ROW_IS_REFERENCED_2"){
-            res.send("<h3> ERROR: " + err.errno + " cannot delete college with ID:" + req.params.college + " as it has assossiated courses");
-        }
-        else{
-            res.send("<h3> ERROR: " + err.errno + " " + error.sqlMessage)
-        }
-             
-    })
+app.get('/listCountries/:country', (req, res) => {
+    mySQLDAO.deleteCountry(req.params.country)
+        .then((result) => {
+            if (result.affectedRows == 0) {
+                res.send("<h3>Country: " + req.params.country + " doesnt exist")
+            }
+            else {
+                res.send("<h3> Country: " + req.params.country + " deleted")
+            }
+            res.send(result);
+        })
+        .catch((err) => {
+            if (err.code == "ER_ROW_IS_REFERENCED_2") {
+                res.send("<h3> ERROR: " + err.errno + " cannot delete college with ID:" + req.params.college + " as it has assossiated courses");
+            }
+            else {
+                res.send("<h3> ERROR: " + err.errno + " " + error.sqlMessage)
+            }
+
+        })
 })
 
-//rendering the add country page
-app.get('/addCountry', (req, res) => {
-    res.render("addCountry");
+app.get('/updateCountry', (req, res) => {
+    res.render("updateCountry")
 })
 
-//rendering the update country page
-app.get('/updateCountry/:country', (req, res) => {
-    home.getCountries(req.params.country)
-    .then((result)=>{
-        console.log(result)
-        res.render("update", {country: result[0]})
-    })
-    .catch((error)=>{
-        res.send(error)
-    })
-})
-
-//post method to post the query result to the server
 app.post("/updateCountry", (req, res) => {
     var myQuery = {
         sql: 'update country set co_name =?, co_details=? where co_code =?',
@@ -113,27 +92,46 @@ app.post("/updateCountry", (req, res) => {
         })
 })
 
-app.get('/updateCountry', (req, res) => {
-    res.render("updateCountry")
+app.get('/addCountry', (req, res) => {
+    res.render("addCountry");
+})
+
+app.get('/newHeadOfState', (req, res) => {
+    res.render("newHeadOfState")
+})
+app.get('/addHeadOfState', (req, res) => {
+    res.render("AddheadOfState")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
 
+// not working --------------------------------------------------------------------------------------------------------------------------------------
 
-//////////////////////////////////////////////////////////////all headOfStare functions
-app.get('/headsOfState', (req, res) => {
-    res.render("headsOfState")
+app.get('/updateCountry/:country', (req, res) => {
+    home.getCountries(req.params.country)
+        .then((result) => {
+            console.log(result)
+            res.render("update", { country: result[0] })
+        })
+        .catch((error) => {
+            res.send(error)
+        })
 })
-app.get('/headsOfState', (req, res) => {
+
+app.get('/newHeadOfState', (req, res) => {
     MongoDAO.getheadsOfState()
-    .then((documents)=>{
-        res.send(documents)
-    })
-    .catch((err)=>{
-        res.send(err)
-    })
+        .then((result) => {
+            console.log(result);
+            res.render('newHeadOfState', { headOfState: result })
+        })
+        .catch((err) => {
+            res.send(err)
+        })
 })
 
-//post method to send the result of the query to the view page
 app.post('/headsOfState', (req, res)=>{
     MongoDAO.getheadsOfState(req.body._id, req.body.headsOfState)
     .then((result)=>{
@@ -143,6 +141,15 @@ app.post('/headsOfState', (req, res)=>{
     })
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.post("/addCountry", (req, res) => {
+    mySQLDAO.addCountry(req.body.co_code, req.body.co_name, req.body.co_details)
+        .then((result) => {
+            console.log(result)
+            res.redirect('/showCountries')
+        })
+        .catch(() => {
+            res.send("Country already exists");
+        })
 })
+
+
--- mySQLDAO.js
+++ mySQLDAO.js
@@ -15,8 +15,6 @@ mysql.createPool({
         console.log(err);
     })
 
-//creating a new promise
-//if query is correct, then go into then , else catch
 var getCities = function () {
     return new Promise((resolve, reject) => {
         pool.query('select * from city')
@@ -29,22 +27,96 @@ var getCities = function () {
     })
 }
 
-var getCity = function(cty_code){
-    return new Promise((resolve , reject)=>{
+var getCity = function (cty_code) {
+    return new Promise((resolve, reject) => {
         var myQuery = {
-            sql:'select * from city where cty_code = ? ',
-            values:[cty_code]
+            sql: 'select * from city where cty_code = ? ',
+            values: [cty_code]
         }
         pool.query(myQuery)
-        .then((result)=>{
-            resolve(result)
-        })
-        .catch((err)=>{
-           // console.log(err)
-            reject(err)
-        })
+            .then((result) => {
+                resolve(result)
+            })
+            .catch((err) => {
+                // console.log(err)
+                reject(err)
+            })
     })
 }
 
-module.exports = { getCities , getCity }
+var getCountries = function (co_code) {
+    return new Promise((resolve, reject) => {
+
+        myQuery = (co_code == undefined ? "select * from country" : "select * from country where co_code = ?;");
+        var queryObj = {
+            sql: myQuery,
+            values: [co_code]
+        }
+        pool.query(queryObj)
+            .then((result) => {
+                resolve(result)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+
+var deleteCountry = function (co_code) {
+    return new Promise((resolve, reject) => {
+        var newQuery = {
+            sql: 'delete from country where co_code = ?',
+            values: [co_code]
+        }
+        pool.query(newQuery)
+            .then((result) => {
+                resolve(result)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+// not working ------------------------------------------------------------------------------------------------------------------------------------
+var updateCountry = function (co_code, co_name, co_details) {
+    //return a new promise
+    return new Promise((resolve, reject) => {
+        //query then sends a result
+        myQuery = (co_code == undefined ? "select * from country where co_code=?" : "update country set co_name =?, co_details=? where co_code =?;");
+        var queryObj = {
+            sql: myQuery,
+            values: [co_code, co_name, co_details]
+        }
+        pool.query(queryObj)
+            .then((result) => {
+                resolve(result)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+var addCountry = function (co_code, co_name, co_details) {
+    //return a new promise
+    return new Promise((resolve, reject) => {
+        //query then sends a result
+        var myQuery = {
+            sql: 'INSERT INTO country VALUES (?,?,?)',
+            values: [co_code, co_name, co_details]
+        }
+        pool.query(myQuery)
+            .then((result) => {
+                resolve(result)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+
+module.exports = { getCities, getCity, getCountries, deleteCountry, updateCountry, addCountry }
 
